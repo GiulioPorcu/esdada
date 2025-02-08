5ff880d0b0bee3ef24740ea1b8c5ee3446a7c254
Giulio Porcu <giulioporcu+github@outlook.com>
2025-02-08 09:35:42 +0100

    Added updated /docs

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -25,7 +25,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "09a8785d8aa359840080e985912984c3
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "4b2350e14c6650ba82871f60906437ea",
-"flutter_bootstrap.js": "75b09aebae49f3fa5eab923af9a12310",
+"flutter_bootstrap.js": "20250c5fb119df3ea12b293d99f91e9e",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
